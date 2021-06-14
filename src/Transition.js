1ff7c4e35236ca98f4934871c7653d6255f8e5b9
velykitty <jiyunee13@gmail.com>
2021-06-14 21:44:21 +0900

    slide both sides direction to right doesn't work well

--- src/Transition.js
+++ src/Transition.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Route, Switch, useLocation} from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import Momentum from './momentum/Momentum';
@@ -6,12 +6,24 @@ import Calendar from './calendar/Calendar';
 import './Transition.scss';
 
 const Transition = () => {
-    const location = useLocation();
+  const [direction, setDirection] = useState('slideToLeft')
+  const location = useLocation();
+
+  const getDirection = () => {
+    if(window.location.pathname !== '/') {
+      setDirection('slideToLeft');
+    } else { setDirection('slideToRight'); }
+  }
+
+  useEffect(() => {
+    getDirection();
+    console.log(direction, window.location.pathname);
+  }, [direction, location])
 
   return (
       <TransitionGroup className="Transition-group">
-        <CSSTransition key={location.pathname} classNames="slide" timeout={{enter: 1000, exit: 1000}}>
-        {/* mountOnEnter={true} unmountOnExit={true} */}
+        <CSSTransition key={location.pathname} 
+                        classNames={direction} timeout={{enter: 1000, exit: 1000}}>
             <Switch location={location}>
                 <Route path="/" exact><Momentum /></Route>
                 <Route path="/calendar" exact><Calendar /></Route>
